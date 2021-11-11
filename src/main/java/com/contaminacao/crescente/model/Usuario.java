db5361a3eea45e5aaffedfc1db80dc213e065ef5
Isaac-Leonel <isaacleonel@frwk.com.br>
2021-11-11 16:44:04 -0300

    Update

--- src/main/java/com/contaminacao/crescente/model/Usuario.java
+++ src/main/java/com/contaminacao/crescente/model/Usuario.java
@@ -5,24 +5,58 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
-@Setter
-@Getter
 public class Usuario {
 
-    
-    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private static final long serialVersionUID = 1L;
     private long id;
-
+    
     private String nome;
     private String email;
     private String senha;
     private String data;
     private String cpf;
 
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
 }
