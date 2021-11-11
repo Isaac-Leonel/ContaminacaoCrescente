0874a3f843601a60b90cfad1afa41811f39d6269
Isaac-Leonel <isaacleonel@frwk.com.br>
2021-11-11 17:16:05 -0300

    Update

--- src/main/java/com/contaminacao/crescente/controllers/UsuarioController.java
+++ src/main/java/com/contaminacao/crescente/controllers/UsuarioController.java
@@ -1,8 +1,27 @@
 package com.contaminacao.crescente.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.contaminacao.crescente.model.Usuario;
+import com.contaminacao.crescente.services.UsuarioService;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
     
+    @Autowired
+    private UsuarioService service;
+    
+    @PostMapping("/salvar-Usuario")
+    public String salvarUsuario(@RequestBody Usuario usuario){
+
+        return service.salvarUsuario(usuario);
+    }
+
 }
--- src/main/java/com/contaminacao/crescente/services/UsuarioService.java
+++ src/main/java/com/contaminacao/crescente/services/UsuarioService.java
@@ -1,8 +1,23 @@
 package com.contaminacao.crescente.services;
 
+import com.contaminacao.crescente.model.Usuario;
+import com.contaminacao.crescente.repository.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
     
+    @Autowired
+    private UsuarioRepository repository;
+
+    public String salvarUsuario(Usuario usuario){
+        try {
+            repository.save(usuario);
+            return "Deu certo!";
+        } catch (Exception e) {
+            return "Deu errado gay!";
+        }
+    }
 }
