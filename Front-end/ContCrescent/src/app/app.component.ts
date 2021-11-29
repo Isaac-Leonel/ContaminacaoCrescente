9ea58ff5dc73be52d35c8e6a305bdd9fc6acae8a
Isaac-Leonel <isaacleonel@frwk.com.br>
2021-11-29 15:31:44 -0300

    Front Update

--- Front-end/ContCrescent/src/app/app.component.ts
+++ Front-end/ContCrescent/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ContCrescent';
+  imglogo = './assets/LOGO.svg';
 }
--- Front-end/ContCrescent/src/app/app.module.ts
+++ Front-end/ContCrescent/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputLoginComponent } from './input-login/input-login.component';
 import { ButtonLoComponent } from './button-lo/button-lo.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputLoginComponent,
-    ButtonLoComponent
+    ButtonLoComponent,
+    LoginPageComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
