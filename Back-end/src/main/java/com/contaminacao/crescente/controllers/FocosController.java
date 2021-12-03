f8c4f3df8360e1820d0d06bd160894be7b922152
Shait Moreira <juniorshait@gmail.com>
2021-12-03 10:39:46 -0300

    Update back 3.0

--- Back-end/src/main/java/com/contaminacao/crescente/controllers/FocosController.java
+++ Back-end/src/main/java/com/contaminacao/crescente/controllers/FocosController.java
@@ -1,13 +1,14 @@
 package com.contaminacao.crescente.controllers;
 
 import java.util.List;
+<<<<<<< Updated upstream
 
 import com.contaminacao.crescente.dto.PontoReferenciaDTO;
+=======
+>>>>>>> Stashed changes
 import com.contaminacao.crescente.services.FocoService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +24,14 @@ public class FocosController {
     private FocoService service;
 
     @GetMapping("/longlat/{lng}/{lat}")
+<<<<<<< Updated upstream
     public List<Object> validarUsuario(@PathVariable("lng") String lng, @PathVariable("lat") String lat) {
 
+=======
+
+    public List<Object> validarUsuario(@PathVariable("lng") String lng, @PathVariable("lat") String lat) {
+       System.out.println(lng + " " + lat);
+>>>>>>> Stashed changes
         return service.verificarLocal(lng, lat);
     }
 
