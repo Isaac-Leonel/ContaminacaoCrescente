53a92e4429256629fbe3bbcab54a9c686e8c28b3
Isaac-Leonel <isaacleonel@frwk.com.br>
2021-12-02 21:09:13 -0300

    update Front

--- Back-end/src/main/java/com/contaminacao/crescente/repository/FocosRepository.java
+++ Back-end/src/main/java/com/contaminacao/crescente/repository/FocosRepository.java
@@ -3,11 +3,10 @@ package com.contaminacao.crescente.repository;
 import com.contaminacao.crescente.model.Focos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FocosRepository extends JpaRepository<Focos, Long>{
-    
-    @Query(value = "SELECT `longitude`, `latitude`, `ponto_referencia` FROM `rio`", nativeQuery = true)
+
+
 }
--- Front-end/ContCrescent/src/app/novo-foco/novo-foco.component.ts
+++ Front-end/ContCrescent/src/app/novo-foco/novo-foco.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { ContServiceService } from './../cont-service.service';
+import { Foco } from './foco.model';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-novo-foco',
@@ -8,28 +10,39 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class NovoFocoComponent implements OnInit {
 
-  causas: String[] = ["Teste","Teste2","Teste3"]  
+  lat: number = 0;
+  lng: number = 0;
 
-  profileForm = this.fb.group({
-    nome: ['', Validators.required],
-    email: ['', Validators.required],
-    dataN: ['', Validators.required],
-    CPF: ['', Validators.required],
-    senha: ['', Validators.required],
-  });
+  local:Foco = new Foco();
+  result: string = '';
 
-  onSubmit() {
-    if(this.profileForm.invalid){
-      alert("Gentileza preencher todo o formulario!")
-    }else{
-      console.log(this.profileForm.value);
-    }
+  ngOnInit() {
+    this.getUserLocation();
+ }
+
+ getUserLocation() {
+    if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(position => {
+         this.lat = position.coords.latitude;
+         this.lng = position.coords.longitude;
+         this.local.lat = this.lat + "";
+         this.local.lng = this.lng + "";
+       });
+      }
+      else {
+        alert("Impossivel encontrar localização! Gentileza permitir uso para melhor experiencia com aplicativo.")
+      }
   }
 
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
+  enviarFoco(){ 
+    this.service.enviarLocal(this.local).subscribe(loc =>{
+      this.local = new Foco();
+      this.result = loc;
+      
+    })
   }
+
+  constructor(private service:ContServiceService, @Inject(DOCUMENT)private document:any) {}
+
 
 }
